f80b90572d3404f008ad90aa2696d340aa457b08
Victor Varela <victor398@gmail.com>
2026-08-11 11:02:39 -0300

    feat: implement project update functionality in EditProjectForm; enhance API integration with error handling

--- uptask_frontend/src/Views/EditProjectForm.tsx
+++ uptask_frontend/src/Views/EditProjectForm.tsx
@@ -1,23 +1,46 @@
+import { updateProject } from "@/api/ProjectAPI";
 import ProjectForm from "@/components/projects/ProjectForm";
 import type { ProjectFormData } from "@/types";
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 type EditProjectFormProps={
   data:ProjectFormData
 }
 
 export default function EditProjectForm({data}:EditProjectFormProps) {
-  
+
+  //Obtenemos el id de la URL, tambien lo pude haber pasado por pops..
+  const params = useParams()
+  const projectId = params.projectId
+  if(!projectId) return <p>Proyecto no encontrado</p>
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ defaultValues: data });
 
+  //Usamos mutations para el PUT
+  const {mutate}= useMutation({
+    mutationFn: updateProject,
+    onError:()=>{
+
+    },
+    onSuccess:()=>{
+
+    }
+  })
+
 
   const handleForm = (formData: ProjectFormData) => {
-    
+    //Por defecto mutate toma 1 solo argumento. por eso armamos un obj para pasar los 2 argumentos que necesitamos. 
+    const data={
+      projectId,
+      formData 
+    }
+    return mutate(data) //este data viaja internamente a traves de mutate desde aca hacia mutationFn:updateProject
   };
   return (
     <>
@@ -49,9 +72,9 @@ export default function EditProjectForm({data}:EditProjectFormProps) {
  * Cuando envio la prop data a EditProjectForm, le estoy pasando un parametro a esa funcion. un componente es una funcion y Ts se queja, subraya en rojo cuando los parametros de funciones no estan tipados ni son inferibles de otra forma. Por eso, en EditProjectForm y como SIEMPRE debe ser que cuando un componente recibe una prop HAY que declara su type- 
  * Nota que reutilizamos el formulario, pero para EDITAR, como ya tenemos data, la usamos para los defautlValues de useForm, ahí esta la clave de esto.. llenar en automatico el form
  * 
+ * En EditProjectView usamos useQuery para GET/obtener data del porject a editar y mostrarlo en este componente EditProjectForm.- Y usamos useMutation para PUT/modificar el project. Fijate las vueltas que dio el profe para obtener el projectId a traves de useParamas otra vez- Mutate() toma solo 1 parametro, por eso armamos un objeto con las 2 cosas que necesita la mutacion, la data del form y el projectId.
  * 
- * 
- * 
+ * Cuando disparamos la mutacion --> mutate(data)---> en mutateFn ejecutamos la api fn y luego manejamos los errores / notificacion a la UI
 
 
 
--- uptask_frontend/src/api/ProjectAPI.ts
+++ uptask_frontend/src/api/ProjectAPI.ts
@@ -18,11 +18,10 @@ export async function getProjects() {
   try {
     const { data } = await api("/projects");
     //validamos la respuesta-> uso el schema de dashboardView
-    const response = dashboardViewSchema.safeParse(data)
-    if(response.success){
-      return response.data
+    const response = dashboardViewSchema.safeParse(data);
+    if (response.success) {
+      return response.data;
     }
-
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
@@ -30,26 +29,37 @@ export async function getProjects() {
   }
 }
 
-export async function editProjectById(id:Project['_id']) {
+export async function editProjectById(id: Project["_id"]) {
   try {
-    const {data} = await api(`/projects/${id}`)
-    return data
-    
+    const { data } = await api(`/projects/${id}`);
+    return data;
   } catch (error) {
-    if(isAxiosError(error) && error.response){
+    if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
-      
     }
   }
-  
+}
+type UpdateProjectType={
+  projectId:Project['_id'],
+  formData:ProjectFormData
+}
+export async function updateProject({projectId, formData}: UpdateProjectType) {
+  try {
+    const { data } = await api.put(`/projects/${projectId}`, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
 }
 
 /**
  * Antes esta funcion no retornaba nada PERO despues le metimos su return para llevar data a handleForm y saber que hay que avisar/notificar a user.
  * El console.log es asi porque asi es la forma de la respuesta de axios, cuando hay error lo obtenemos desde response.data.error--> ahi se queja Ts porque no sabe que es error 'error' is of type 'unknown'. El que sí sabe que tipo es error es AXIOS y axios tiene una funcion que evalua si es un error de axios, entonces USAMOS esa Fn y && verificamos que error.response tenga algo y de esa manera hacemos feliz a Ts : ) || entonces en la respuesta de la api podemos tener data o se va por el catch y tenemos error-- si va por error debemos verificar si es AxiosError para hacer feliz a Ts y && que error.response tenga algo-> si eso se cumple LANZO un nuevo error trow new Error que sabemos que esta en error.response.data.error--> eso lo vamos a pasar a la vista en la mutacion y ahi hacemos toast a error.message-< IMPORTANTE si no hacemos trow new Error, en la vista no va a ser tratado como tal, y no va a entrar al onError de la mutacion--< Faltaria escribir el else de if (isAxiosError()) porque puede haber un error de red o algo inesperado y nunca llege a ejectuar el onError o el onSuccess... ojo con eso.
- * 
+ *
  * Para la Fn getProjects, usamos el schema de dashboardView para validar que la respuesta tiene los campos y los types correctos, eso elimina los any que teniamos en DashboardView.ts. nos aseguramos que pasen la validacion de ZOD y ahi Retornamos la respuesta ya validada a la vista. sintaxis===== dashboardViewSchema.safeParse(data) SAFEPARSE°°°
- * 
+ *
  * Para editProjectById usamos lookup del type Project ==> editProjectById(id:Project['_id'])- la url es la MISMA que definimos en el BACKEND. las URLs de las funcines que llaman a la API son OBVIAMENTE las mismas que definimos en el Backend, me habia confundido y puse la URL del frontend en su lugar instead.
  *
  *
